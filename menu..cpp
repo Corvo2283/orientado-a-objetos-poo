a72edb1aecace6c22c8b88cc392ed010b90583cb
Corvo2283 <leoismaflores@gmail.com>
2019-11-01 19:17:19 -0600

    se subio el menu

--- menu..cpp
+++ menu..cpp
@@ -1,22 +1,70 @@
 #include <iostream>
-#include <stdlib.h>
 
 using namespace std;
-//se creara un menu de opciones
-int main(){
+
+class Menu{
 private:
-    
+    int opcion,salir;
+public:
+    Menu();
+    void show();
+    int Salir();
+};
 
+Menu::Menu(){
+}
+void Menu::show(){
+    cout<<"***MENU DE OPERACIONES***"<<endl;
+    cout<<"1.- Suma"<<endl;
+    cout<<"2.- Resta"<<endl;
+    cout<<"3.- Multiplicaicon"<<endl;
+    cout<<"4.- Divicion"<<endl;
+    cout<<"5.- Salir"<<endl;
+    cout<<"Opcion: ";
+    cin>>opcion;
 
+        switch (opcion){
+        case 1:
+            cout<<"Seleccionaste Suma ¿Estas Seguro?"<<endl;
+            cin.ignore();
+            salir = 0;
+            break;
+        case 2:
+            cout<<"Seleccionaste Resta ¿Estas Seguro?"<<endl;
+            cin.ignore();
+            salir = 0;
+            break;
+        case 3:
+            cout<<"Seleccionaste Multiplicacion ¿Estas Seguro?"<<endl;
+            cin.ignore();
+            salir = 0;
+            break;
+        case 4:
+            cout<<"Seleccionaste Divicion ¿Estas Seguro?"<<endl;
+            cin.ignore();
+            salir = 0;
+            break;
+        case 5:
+            salir = 1;
+            break;
+        default:
+            cout<<"La opciÃ³n no existe, intentelo de nuevo"<<endl<<endl;
+            salir = 0;
+            break;
+        }
+}
 
+int Menu::Salir(){
+    return salir;
+}
 
+int main(){
 
+    Menu menu1;
 
+    do{
+        menu1.show();
+    }while (menu1.Salir() == 0);
 
-
-
-
-
-
-    system("pause");
+return 0;
 }
